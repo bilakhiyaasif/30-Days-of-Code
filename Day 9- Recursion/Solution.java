4e1907b391595281952de7dd216aad8e226df2a1
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2017-12-25 01:46:13 +0530

    Create Solution.java

---
+++ Day 9- Recursion/Solution.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int factorial(int n) {
+        // Complete this function
+        if(n<=1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n*factorial(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(2<=n&&12>=12)
+        {
+            
+        
+        int result = factorial(n);
+        System.out.println(result);
+        }
+   }
+}
