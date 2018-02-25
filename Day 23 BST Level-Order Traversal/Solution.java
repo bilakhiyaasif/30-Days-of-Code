d47ea986d149bf83e2a00a7412013d474aa699c3
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-02-25 22:21:40 +0530

    Create Solution.java

---
+++ Day 23 BST Level-Order Traversal/Solution.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+  static void levelOrder(Node root){
+
+    int h = height(root);
+    int i;
+    for (i=1; i<=h; i++)
+      printGivenLevel(root, i);
+
+      }
+      public static void printGivenLevel (Node root ,int level)
+    	{
+    		if (root == null)
+    			return;
+    		if (level == 1)
+    			System.out.print(root.data + " ");
+    		else if (level > 1)
+    		{
+    			printGivenLevel(root.left, level-1);
+    			printGivenLevel(root.right, level-1);
+    		}
+    	}
+      static int height(Node root)
+      {
+        if (root == null)
+        return 0;
+        else
+        {
+          /* compute height of each subtree */
+          int lheight = height(root.left);
+          int rheight = height(root.right);
+
+          /* use the larger one */
+          if (lheight > rheight)
+            return(lheight+1);
+          else return(rheight+1);
+        }
+      }
+      public static Node insert(Node root,int data){
+      if(root==null){
+          return new Node(data);
+      }
+      else{
+          Node cur;
+          if(data<=root.data){
+              cur=insert(root.left,data);
+              root.left=cur;
+          }
+          else{
+              cur=insert(root.right,data);
+              root.right=cur;
+          }
+          return root;
+      }
+  }
+  public static void main(String args[]){
+          Scanner sc=new Scanner(System.in);
+          int T=sc.nextInt();
+          Node root=null;
+          while(T-->0){
+              int data=sc.nextInt();
+              root=insert(root,data);
+          }
+          levelOrder(root);
+      }
+}
